a19b08a67bd6b8fd5558831e70433081cb0daa72
Rafael Alonso <rafael.palonso@gmail.com>
2021-05-26 10:58:51 -0300

    routes advanced

---
+++ app/models/review.rb
@@ -0,0 +1,3 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant # @review.restaurant
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # se vou fazer algo referente aos restaurantes, coloco dentro do "do ... end" do resources :restaurants
+  resources :restaurants do
+    # Vou fazer algo referente a VARIOS restaurantes ou referente a UM restaurante?
+    # - fazer para vários => COLLECTION
+    collection do
+      get :top
+    end
+
+    # - fazer para um:
+    # - Vou fazer algo para UM restaurante ou vou fazer algo para um modelo relacionado ao restaurante?
+    # -- fazer para a review => MEMBER
+    member do
+      get :chef
+    end
+
+    # -- fazer para modelo relacionado => RESOURCES
+    resources :reviews, only: [:new, :create]
+  end
+  
+  # quero deletar reviews => destroy (V)
+  resources :reviews, only: [:destroy]
 end
